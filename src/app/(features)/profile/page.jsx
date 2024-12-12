407805a84db006887932ddc263981cc4e2da7eff
banala sai sathwik <banalasaisathwik@gmail.com>
2024-12-12 11:51:43 +0530

    secong change

--- src/app/(features)/profile/page.jsx
+++ src/app/(features)/profile/page.jsx
@@ -5,6 +5,8 @@ const Admin = () => {
         <div style={{ padding: "20px" }}>
             <h1>Admin Dashboard</h1>
             <p>Welcome to the Admin Dashboard. Manage your application here.</p>
+
+            <p> second change sathwik</p>
         </div>
     );
 };
