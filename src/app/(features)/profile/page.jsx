6c9b2a030324a6db23a112375e511f44a3b67c60
Tarun7386 <nellikudurutarun1234@gmail.com>
2024-12-12 12:00:01 +0530

    added new

--- src/app/(features)/profile/page.jsx
+++ src/app/(features)/profile/page.jsx
@@ -5,6 +5,7 @@ const Admin = () => {
         <div style={{ padding: "20px" }}>
             <h1>Admin Dashboard</h1>
             <p>Welcome to the Admin Dashboard. Manage your application here.</p>
+            <p> add my profile changes in development</p>
         </div>
     );
 };
