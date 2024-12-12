7fd76fcba604a956ee25963bb3502012b0578242
Tarun7386 <nellikudurutarun1234@gmail.com>
2024-12-12 11:42:48 +0530

    from tarun

--- src/app/(features)/profile/page.jsx
+++ src/app/(features)/profile/page.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
     return (
         <div style={{ padding: "20px" }}>
             <h1>Profile</h1>
-            
+            <h2>i added h2 tag</h2>
             <p>Welcome to the Profile page. Manage your application here.</p>
         </div>
     );
